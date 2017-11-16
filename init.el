bd52ea05e3046db450df43fb5e18ba8470743433
corey ducharme <corey ducharme>
2017-11-15 22:25:45 -0500

    Use custom file for changes generated by emacs

--- init.el
+++ init.el
@@ -156,24 +156,10 @@
   (dashboard-setup-startup-hook))
 
 
-
 ;; Projectile project management
 (use-package projectile)
 
 
-;; This section is generated automatically by emacs
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (ox-latex-chinese org haskell-mode nlinum which-key try use-package doom-themes)))
- '(show-paren-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack" :foundry "simp" :slant normal :weight normal :height 113 :width normal)))))
+;; Set custom file that is modified automatically by customization done through emacs
+(setq custom-file "~/.emacs.d/custom.el")
+(load-file custom-file)
