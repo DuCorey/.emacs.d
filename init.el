98c2c0a660405942b1713e92885912727cf3d906
corey ducharme <corey ducharme>
2017-11-16 22:49:56 -0500

    Git gutter mode
    Still requires work for refresh time

--- init.el
+++ init.el
@@ -20,6 +20,42 @@
   (find-file user-init-file))
 
 
+;; Macros
+(defmacro add-hook! (hook &rest func-or-forms)
+  "A convenience macro for `add-hook'.
+HOOK can be one hook or a list of hooks. If the hook(s) are not quoted, -hook is
+appended to them automatically. If they are quoted, they are used verbatim.
+FUNC-OR-FORMS can be a quoted symbol, a list of quoted symbols, or forms. Forms will be
+wrapped in a lambda. A list of symbols will expand into a series of add-hook calls.
+Examples:
+    (add-hook! 'some-mode-hook 'enable-something)
+    (add-hook! some-mode '(enable-something and-another))
+    (add-hook! '(one-mode-hook second-mode-hook) 'enable-something)
+    (add-hook! (one-mode second-mode) 'enable-something)
+    (add-hook! (one-mode second-mode) (setq v 5) (setq a 2))"
+  (declare (indent defun) (debug t))
+  (unless func-or-forms
+    (error "add-hook!: FUNC-OR-FORMS is empty"))
+  (let* ((val (car func-or-forms))
+         (quoted (eq (car-safe hook) 'quote))
+         (hook (if quoted (cadr hook) hook))
+         (funcs (if (eq (car-safe val) 'quote)
+                    (if (cdr-safe (cadr val))
+                        (cadr val)
+                      (list (cadr val)))
+                  (list func-or-forms)))
+         (forms '()))
+    (mapc
+     (lambda (f)
+       (let ((func (cond ((symbolp f) `(quote ,f))
+                         (t `(lambda (&rest _) ,@func-or-forms)))))
+         (mapc
+          (lambda (h)
+            (push `(add-hook ',(if quoted h (intern (format "%s-hook" h))) ,func) forms))
+          (-list hook)))) funcs)
+`(progn ,@forms)))
+
+
 ;; Fix C-i being the same as tab
 (setq local-function-key-map (delq '(kp-tab . [9]) local-function-key-map))
 (define-key input-decode-map "\C-i" [C-i])
@@ -171,6 +207,28 @@
 ;; Projectile project management
 (use-package projectile)
 
+
+;; Git gutter display git changes on the left of the window
+(use-package git-gutter
+  :init
+  (add-hook! (text-mode prog-mode conf-mode) 'git-gutter-mode)
+  :ensure git-gutter-fringe
+  :config
+  (require 'git-gutter-fringe)
+
+  ;; NOTE If you want the git gutter to be on the outside of the margins (rather
+  ;; than inside), `fringes-outside-margins' should be non-nil.
+
+  ;; colored fringe "bars"
+  (define-fringe-bitmap 'git-gutter-fr:added
+    [224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224]
+    nil nil 'center)
+  (define-fringe-bitmap 'git-gutter-fr:modified
+    [224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224]
+    nil nil 'center)
+  (define-fringe-bitmap 'git-gutter-fr:deleted
+    [0 0 0 0 0 0 0 0 0 0 0 0 0 128 192 224 240 248]
+    nil nil 'center)
 
   ;; Refresh git gutter
   (setq git-gutter:update-interval 0.2))
