651b9773e0834dab96e5ae15bcfe657015732d9e
Corey Ducharme <corey.ducharme@polymtl.ca>
2019-06-12 10:48:17 -0400

    Load org-journal update agenda when loading the dashboard
    Change agenda to weekly

--- modules/module-dashboard.el
+++ modules/module-dashboard.el
@@ -4,12 +4,15 @@
 
 (use-package dashboard
   :config
+  (require 'org-journal)
+  (org-journal-update-org-agenda-files)
   (dashboard-setup-startup-hook)
   :custom
   (dashboard-items '((recents . 5)
 		     (bookmarks . 5)
 		     (projects . 5)
-		     (agenda . 5))))
+		     (agenda . 5)))
+  (show-week-agenda-p t))
 
 
 (provide 'module-dashboard)
