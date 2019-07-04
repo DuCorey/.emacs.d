76f4d603ff0f2bcd71246cd923bf392be4fb2b73
Corey Ducharme <corey.ducharme@polymtl.ca>
2019-07-04 11:51:12 -0400

    Clean up neotree

--- modules/module-neotree.el
+++ modules/module-neotree.el
@@ -4,19 +4,15 @@
 
 ;;; Code:
 
-;;;(global-set-key [f5] 'neotree-toggle)
 (use-package neotree
   :bind (("<f5>" . neotree-toggle))
   :config
   (setq-default neo-show-hidden-files t)
-
+  :hook
   ;; Disable line-numbers minor mode for neotree
-  (add-hook 'neo-after-create-hook
-	    (lambda (&optional dummy) (display-line-numbers-mode -1)))
-
+  (neo-after-create . (lambda (&optional dummy) (display-line-numbers-mode -1)))
   :custom
   (inhibit-compacting-font-caches t)
-
   ;; Every time when the neotree window is opened, let it find current file and jump to node.
   (neo-smart-open t))
 
