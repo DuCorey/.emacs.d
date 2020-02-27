d3c0ce605e0ac7d90c0b328313988ddd4c1ab144
Corey Ducharme <corey.ducharme@polymtl.ca>
2020-02-27 16:05:56 -0500

    Straight package manager
    Currently made to work alongside package.el, maybe in the future we
    only use straight

--- core/core-bootstrap.el
+++ core/core-bootstrap.el
@@ -35,6 +35,22 @@
 ;; Global ensure for use-package
 (setq use-package-always-ensure t)
 
+;; Bootstrap for 'straight
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(setq straight-use-package-by-default nil)
+
 
 (provide 'core-bootstrap)
 
