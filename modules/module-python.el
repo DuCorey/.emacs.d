a64a0b4ef150af66dfc2d67a44d35d4959f5fa5c
Corey Ducharme <corey@ducharme.engineer>
2021-07-16 11:14:24 -0400

    Revert python mode to defaults

--- modules/module-python.el
+++ modules/module-python.el
@@ -7,8 +7,8 @@
 ;; Major mode settings for python
 ;; Use conda's root python env
 ;; This is incompatible with pyvevn
-(setq python-shell-interpreter "python"
-      python-shell-interpreter-args "console --simple-prompt")
+(setq python-shell-interpreter "python")
+;;      python-shell-interpreter-args "console --simple-prompt")
 
 ;; elpy adds a lot of modules for working with python
 (use-package elpy
@@ -16,19 +16,20 @@
   :config
   (elpy-enable)
   :custom
-  (elpy-modules (quote
-		 (elpy-module-sane-defaults elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-yasnippet))))
+  (elpy-modules '(elpy-module-sane-defaults
+		  elpy-module-company
+		  elpy-module-eldoc
+		  elpy-module-yasnippet)))
 
 ;; Virtual environment management for python
 (use-package pyvenv
   :defer t
   :config
-  (cond (windows-system (setenv "WORKON_HOME" (concat "C:" (getenv "HOMEPATH") "\\Miniconda3\\envs")))
-	(linux-system
-	 (setenv "WORKON_HOME" "~/miniconda3/envs")
-	 (setenv "IPY_TEST_SIMPLE_PROMPT" "1")))
-  (pyvenv-mode 1)
-  (pyvenv-workon-home))
+  ;; (cond (windows-system (setenv "WORKON_HOME" (concat "C:" (getenv "HOMEPATH") "\\Miniconda3\\envs")))
+  ;; 	(linux-system
+  ;; 	 (setenv "WORKON_HOME" "~/miniconda3/envs")
+  ;; 	 (setenv "IPY_TEST_SIMPLE_PROMPT" "1")))
+  (pyvenv-mode 1))
 ;; TODO fix virtual envs menu to only show up for python files
 
 
