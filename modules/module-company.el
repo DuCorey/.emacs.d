a1589de7b615ad79a292d4ccc95030903b50729f
Corey Ducharme <corey@ducharme.engineer>
2021-07-16 11:11:50 -0400

    Company mode inmprovements

--- modules/module-company.el
+++ modules/module-company.el
@@ -8,13 +8,16 @@
   :hook
   (after-init . global-company-mode)
   :custom
-  (company-idle-delay 0.1)
+  (company-idle-delay 0.2)
   (company-minimum-prefix-length 3)
   (company-show-numbers t)
   (company-global-modes '(not inferior-python-mode))
   ;; Company dabbrev recommendations are case sensitive
-  (company-dabbrev-downcase nil))
-
+  (company-dabbrev-downcase nil)
+  ;; Avoid duplicates in company mode when use case sensitive mode
+  (company-dabbrev-ignore-case nil)
+  ;; Company dabbrev only search buffers with the same major mode
+  (company-dabbrev-other-buffers t))
 
 (provide 'module-company)
 
