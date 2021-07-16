2500c9680752dc78762ded86769f86f029d6f49b
Corey Ducharme <corey@ducharme.engineer>
2021-07-16 11:13:23 -0400

    Add single parentheses insert

--- modules/module-smartparens.el
+++ modules/module-smartparens.el
@@ -7,8 +7,10 @@
 (use-package smartparens
   :config
   (require 'smartparens-config)
-  :hook (prog-mode . smartparens-mode))
-
+  :hook (prog-mode . smartparens-mode)
+  ;; Alllow for single insert of pair objects
+  :bind (("C-(" . (lambda () (interactive) (insert "(")))
+	 ("C-)" . (lambda () (interactive) (insert ")")))))
 
 (provide 'module-smartparens)
 
