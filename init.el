defeed33a4bbd96b466993a4b7eef827673ab8ed
Corey Ducharme <corey@ducharme.engineer>
2023-02-03 13:38:13 -0500

    Implement Json module

--- init.el
+++ init.el
@@ -90,6 +90,7 @@
 (require 'module-prescient)
 (require 'module-smartparens)
 (require 'module-markdown)
+(require 'module-json)
 
 ;;(require 'josh-chinese)
 
---
+++ modules/module-json.el
@@ -0,0 +1,12 @@
+;;; module-json --- Mode for editing JSON files
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package json-mode)
+
+(provide 'module-json)
+
+
+;;; module-json ends here
